0d23929cff9da2df0981a788f0c1eda6611f2393
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-06-09 04:27:19 +0300

    pass context to Add Task

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,8 +1,10 @@
 import './AddTask.css'
 import { useState } from 'react'
+import { useTask } from './TaskProvider'
 
-export default function AddTask(props){
+export default function AddTask(){
 	const [task,setTask] = useState('')
+	const {handleAdd} = useTask()
 
 	const handleOnchange = e => {
 		setTask(e.target.value)
@@ -10,14 +12,16 @@ export default function AddTask(props){
 
 	const handleKeyDown = e =>{
 		if(e.key === 'Enter')
-			props.handleAdd(task)
+		handleAdd(task)
 	}
+
+	console.log("AddTask rendered")
 
 	return (
 		<div className='AddTask'>
 			<input placeholder='Add task here' className='taskinput'
 			onChange={handleOnchange} onKeyDown={handleKeyDown}/>
-			<button className='AddTaskButton' onClick={()=>props.handleAdd(task)}>Add Task</button>
+			<button className='AddTaskButton' onClick={()=>handleAdd(task)}>Add Task</button>
 		</div>
 	)
 	
